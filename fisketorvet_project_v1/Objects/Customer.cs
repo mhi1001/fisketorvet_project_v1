f6cd02b7b9e8cba74eea2f13f82d3608f1ee7530
Paula Catalina Curca <paul6465@edu.easj.dk>
2020-11-25 13:30:05 +0100

    s

--- fisketorvet_project_v1/Objects/Customer.cs
+++ fisketorvet_project_v1/Objects/Customer.cs
@@ -7,5 +7,11 @@ namespace fisketorvet_project_v1.Pages
 {
     public class Customer
     {
+        private int _id;
+        private string _name;
+        private string _address;
+        private string _bankNo;
+        private string _email;
+        private string _pass;
     }
 }
