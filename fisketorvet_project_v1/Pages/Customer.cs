fb8458551819513dbc9fcbb986edd98a124f3a2b
AlexandruLazaroiu <alexandru_lazaroiu94@yahoo.com>
2020-11-25 13:19:06 +0100

    added customer class

---
+++ fisketorvet_project_v1/Pages/Customer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace fisketorvet_project_v1.Pages
+{
+    public class Customer
+    {
+    }
+}
