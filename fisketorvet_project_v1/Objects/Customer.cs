4cfa0e1d0c3a39aa8691288ead77f7b710800e00
Paula Catalina Curca <paul6465@edu.easj.dk>
2020-11-25 13:33:49 +0100

    yeah

--- fisketorvet_project_v1/Objects/Customer.cs
+++ fisketorvet_project_v1/Objects/Customer.cs
@@ -13,5 +13,37 @@ namespace fisketorvet_project_v1.Pages
         private string _bankNo;
         private string _email;
         private string _pass;
+
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public string BankNo
+        {
+            get { return _bankNo; }
+            set { _bankNo = value; }
+        }
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        public string Password
+        {
+            get { return _pass; }
+            set { _pass = value; }
+        }
     }
 }
