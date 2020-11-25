231e48fdbf9ff989262cbdc496e8bcae47004eaa
Paula Catalina Curca <paul6465@edu.easj.dk>
2020-11-25 13:54:15 +0100

    yeah

---
+++ fisketorvet_project_v1/Catalog/CustCatalog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using fisketorvet_project_v1.Pages;
+
+namespace fisketorvet_project_v1.Catalog
+{
+    public class CustCatalog
+    {
+        Dictionary<int, Customer> clients=new Dictionary<int, Customer>();
+
+        public void AddCustomer(Customer c)
+        {
+            clients.Add(c.ID,c);
+        }
+
+        public void DeleteCustomer(Customer c)
+        {
+            clients.Remove(c.ID);
+        }
+
+        public void Update(Customer c)
+        {
+            c.ID=new int();
+            c.Address=new string(c.Address);
+        }
+    }
+}
