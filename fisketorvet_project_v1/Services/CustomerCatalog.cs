d3ebd570b5f95e0dca1558cd7b033fc182bb912f
Paula Catalina Curca <paul6465@edu.easj.dk>
2020-11-26 10:38:04 +0100

    added methods

--- fisketorvet_project_v1/Services/CustomerCatalog.cs
+++ fisketorvet_project_v1/Services/CustomerCatalog.cs
@@ -8,6 +8,16 @@ namespace fisketorvet_project_v1.Services
         private string filePath = @".\Data\Customers.json";
         private Dictionary<int, Customer> Customers { get; set; }
 
+        public void AddCustomer(Customer c)
+        {
+            Customers.Add(c.Id,c);
+        }
+
+        public void RemoveCustomer(Customer c)
+        {
+            Customers.Remove(c.Id);
+        }
+
 
     }
 }
