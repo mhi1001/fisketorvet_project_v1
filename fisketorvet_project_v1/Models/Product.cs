83b6ab3e8b68868af8f451dc0561fc6f4b733335
Paula Catalina Curca <paul6465@edu.easj.dk>
2020-11-27 10:44:00 +0100

    yess

--- fisketorvet_project_v1/Models/Product.cs
+++ fisketorvet_project_v1/Models/Product.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+public enum ProductType
+{
+    Food,
+    Beverages,
+    Clothing,
+    Medicine,
+    Toys,
+    Books,
+
+}
+
 namespace fisketorvet_project_v1.Models
 {
     public class Product
