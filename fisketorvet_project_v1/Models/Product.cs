1aad4ddcea9eeb70e7b3794f67cefa4e32e25df7
pc <pc@DESKTOP-BI5FP4B>
2020-11-27 10:53:13 +0100

    Added Our Stores page with content. And moved some directories around.

--- fisketorvet_project_v1/Models/Product.cs
+++ fisketorvet_project_v1/Models/Product.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+public enum ProductType
+{
+    Food,
+    Beverages,
+    Clothing,
+    Medicine,
+    Toys,
+    Books,
+
+}
 namespace fisketorvet_project_v1.Models
 {
+
     public class Product
     {
         public int Id { get; set; }
         public ProductType ProductType { get; set; }
         public string ProductName { get; set; }
-        public string Dimensions { get; set; }
+        public string Dimensions { get; set; } //Dimensions can be colour, size, weight, quantity, etc, I just dont know if we
+                                                //should implement all these as variables or leave it all in dimensions
         
     }
 }
