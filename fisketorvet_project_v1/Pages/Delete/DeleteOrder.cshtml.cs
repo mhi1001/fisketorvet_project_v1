41ce8afc8b634bd18c331fd492fc127a25283116
pc <pc@DESKTOP-BI5FP4B>
2020-12-08 14:15:44 +0100

    Added delete functionality for several areas. Added customer management page.

--- fisketorvet_project_v1/Pages/Delete/DeleteOrder.cshtml.cs
+++ fisketorvet_project_v1/Pages/Delete/DeleteOrder.cshtml.cs
@@ -2,26 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using fisketorvet_project_v1.Models;
 using fisketorvet_project_v1.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace fisketorvet_project_v1.Pages.Delete
 {
-    public class DeleteCustomerModel : PageModel
+    public class DeleteOrderModel : PageModel
     {
-        private CustomerCatalog _customerCatalog;
+        private OrderCatalog _orderCatalog;
 
-        public DeleteCustomerModel(CustomerCatalog repoCustomerCatalog)
+        public DeleteOrderModel(OrderCatalog repoOrderCatalog)
         {
-            _customerCatalog = repoCustomerCatalog;
+            _orderCatalog = repoOrderCatalog;
         }
 
-
+        public void OnGet()
+        {
+        }
         public IActionResult OnPost(int id)
         {
-            _customerCatalog.RemoveCustomer(id);
+            _orderCatalog.RemoveOrder(id);
             return Redirect("/AdminSection/OrderAdminPage");
         }
     }
